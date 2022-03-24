06cf872b8a7f38de1bfcf9dc49c2c6a32489b13e
Muhammed Monis <koolmonis@gmail.com>
2022-03-24 22:17:06 +0500

    Updated for Hand Detection Record and Sentences

--- webcam_manager.py
+++ webcam_manager.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import INFINITE
 import cv2
 import numpy as np
 import mediapipe as mp
@@ -62,38 +63,42 @@ class WebcamManager(object):
         acc2=0
         window_w = int(HEIGHT * len(frame[0]) / len(frame))
         list1=[]
+        self.sign_detected=f'Predicted Sign ={self.sign_detected}'
         if self.sign_detected==self.val:
-            self.sign_detected=f'Predicted Correctly With Accuracy ={self.dist[0]}'
+            #self.sign_detected=f'Predicted Correctly With Accuracy ={self.dist[0]}'
             for i in range(len(self.sign)):
                 if self.sign[i]==self.val:
                     list1.append(self.dist[i])
                     if len(list1)==2:
-                        acc1=int(list1[0])
-                        acc2=int(list1[1])
-                        acc1=((acc1-500)/500)*100
-                        acc2=((acc2-500)/500)*100
-                        if int(list1[0])<500 or int(list1[1])<500:
-                            self.sign_detected='Predicted correctly With Accuracy =95'
-                            acc2=95
-                            break
+                        if list1[0] == float('inf') or list1[1] ==float('inf'):
+                            pass
                         else:
-                            if acc1>100 or acc2>100:
-                                acc=((acc1+acc2)/2)//100
-                                self.sign_detected=f'Predicted correctly With Accuracy ={acc}'
-                                acc2=acc
+                            acc1=int(list1[0])
+                            acc2=int(list1[1])
+                            acc1=((acc1-500)/500)*100
+                            acc2=((acc2-500)/500)*100
+                            if int(list1[0])<500 or int(list1[1])<500:
+                                #self.sign_detected='Predicted correctly With Accuracy =95'
+                                acc2=95
                                 break
                             else:
-                                acc=(acc1+acc2)/2
-                                self.sign_detected=f'Predicted correctly With Accuracy ={100-acc}'
-                                acc2=100-acc
-                                break
+                                if acc1>100 or acc2>100:
+                                    acc=((acc1+acc2)/2)//100
+                                    #self.sign_detected=f'Predicted correctly With Accuracy ={acc}'
+                                    acc2=acc
+                                    break
+                                else:
+                                    acc=(acc1+acc2)/2
+                                    #self.sign_detected=f'Predicted correctly With Accuracy ={100-acc}'
+                                    acc2=100-acc
+                                    break
         elif self.sign_detected !=self.val:
             for i in range(len(self.sign)):
                 if self.sign[i]==self.val:
                     list1.append(self.dist[i])
                     if len(list1)==2:
-                        if list1[0]==float('inf'):
-                            self.sign_detected='No Sign Detected'
+                        if list1[0]==float('inf') and list1[1]==float('inf'):
+                            #self.sign_detected='No Sign Detected'
                             acc2=0
                         else:    
                             acc1=int(list1[0])
@@ -101,18 +106,18 @@ class WebcamManager(object):
                             acc1=((acc1-500)/500)*100
                             acc2=((acc2-500)/500)*100
                             if int(list1[0])<500 or int(list1[1])<500:
-                                self.sign_detected='Predicted Incorrectly With Accuracy =95'
+                                #self.sign_detected='Predicted Incorrectly With Accuracy =95'
                                 acc2=95
                                 break
                             else:
                                 if acc1>100 or acc2>100:
                                     acc=((acc1+acc2)/2)//100
-                                    self.sign_detected=f'Predicted Incorrectly With Accuracy ={acc}'
+                                    #self.sign_detected=f'Predicted Incorrectly With Accuracy ={acc}'
                                     acc2=acc
                                     break
                                 else:
                                     acc=(acc1+acc2)//2
-                                    self.sign_detected=f'Predicted Incorrectly With Accuracy ={100-acc}'
+                                    #self.sign_detected=f'Predicted Incorrectly With Accuracy ={100-acc}'
                                     acc2=100-acc
                                     break
 
