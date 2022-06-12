1730600844899f581ce8c988a01fa2fda2d2e643
Monis Jawed <koolmonis@gmail.com>
2022-06-12 15:16:04 +0500

    Added a Re-Enter Password textbox for Verification

--- Password.py
+++ Password.py
@@ -6,17 +6,23 @@ from PyQt5.QtGui import *
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(370, 138)
+        MainWindow.resize(370, 142)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(20, 20, 81, 21))
         self.label.setObjectName("label")
+        self.label2 = QtWidgets.QLabel(self.centralwidget)
+        self.label2.setGeometry(QtCore.QRect(20, 60, 92, 21))
+        self.label2.setObjectName("label2")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(120, 20, 201, 31))
+        self.lineEdit.setGeometry(QtCore.QRect(120, 20, 201, 20))
         self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit2.setGeometry(QtCore.QRect(120, 60, 201, 20))
+        self.lineEdit2.setObjectName("lineEdit")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(150, 60, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(150, 100, 75, 23))
         self.pushButton.setObjectName("pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -26,6 +32,7 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         self.lineEdit.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit2.setEchoMode(QtWidgets.QLineEdit.Password)
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -33,8 +40,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Set Password"))
         self.label.setText(_translate("MainWindow", "Enter Password"))
+        self.label2.setText(_translate("MainWindow", "Re-Enter Password"))
         self.pushButton.setText(_translate("MainWindow", "Submit"))
         self.pushButton.clicked.connect(self.passw)
     def passw(self):
@@ -53,13 +61,36 @@ class Ui_MainWindow(object):
             popup.setIcon(QMessageBox.Critical)
             popup.exec_()
         else:
-            MainWindow.close()  
-            print(str(self.lineEdit.text()))
+            if str(self.lineEdit2.text())=="":
+                popup=QMessageBox()
+                popup.setWindowTitle("Enter Password")
+                popup.setText("Please Enter the Password Again")
+                popup.setStandardButtons(QMessageBox.Ok)
+                popup.setIcon(QMessageBox.Critical)
+                popup.exec_()
+            elif len(str(self.lineEdit2.text()))!= len(str(self.lineEdit.text())):
+                popup=QMessageBox()
+                popup.setWindowTitle("Enter Password")
+                popup.setText("Password Length Does not match the previous Password")
+                popup.setStandardButtons(QMessageBox.Ok)
+                popup.setIcon(QMessageBox.Critical)
+                popup.exec_()
+            elif (self.lineEdit2.text())!= (self.lineEdit.text()):
+                popup=QMessageBox()
+                popup.setWindowTitle("Enter Password")
+                popup.setText("Passwords do not match please Re-Enter Again")
+                popup.setStandardButtons(QMessageBox.Ok)
+                popup.setIcon(QMessageBox.Critical)
+                popup.exec_()
+            else:
+                MainWindow.close()  
+                print(str(self.lineEdit.text()))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon("logo.png"))
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
